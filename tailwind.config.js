d158de29c95bf9ddae19f843178f3d198d5ac2c2
SatrioUtomo13 <satrio.utomo130799@gmail.com>
2022-06-11 02:24:30 +0700

    start login

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-  content: ['./docs/**/*.{html,js}'],
+  content: ['./docs/**/*.{html,js}', './docs/index.html'],
   theme: {
     extend: {
       fontFamily: {
         poppins: ['Poppins'],
+      },
+      colors: {
+        'secondary': '#2d2c2c',
+        'primary': '#F0F0F0',
       },
     },
   },
