d5184ad43ce67df47de768b12afad6d695046075
SatrioUtomo13 <satrio.utomo130799@gmail.com>
2022-07-09 05:44:23 +0700

    confirm warning

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,8 @@
-logout = document.getElementById("logout");
+// konfirmasi logout
+const logout = document.getElementById("logout");
+const konfirmasi = document.getElementById("confirm");
+const main = document.getElementById("main");
+logout.addEventListener('click', function(){
+    konfirmasi.style.display = 'flex';
+    main.classList.add('blur');
+})
