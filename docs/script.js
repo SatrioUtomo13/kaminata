3ed40ab9dee746a0e3f93bf4bec2f63c4ee9657a
SatrioUtomo13 <satrio.utomo130799@gmail.com>
2022-07-11 11:45:35 +0700

    fix home

--- docs/script.js
+++ docs/script.js
@@ -2,7 +2,12 @@
 const logout = document.getElementById("logout");
 const konfirmasi = document.getElementById("confirm");
 const main = document.getElementById("main");
+const close = document.getElementById("close");
 logout.addEventListener('click', function(){
     konfirmasi.style.display = 'flex';
     main.classList.add('blur');
+})
+close.addEventListener('click', function(){
+    konfirmasi.style.display = 'none';
+    main.classList.remove('blur');
 })
