348dbef8af0fe865b2d0aec2be9aaecdf3f084a2
SatrioUtomo13 <satrio.utomo130799@gmail.com>
2022-08-01 01:12:37 +0700

    clear

--- docs/script.js
+++ docs/script.js
@@ -5,18 +5,4 @@ profile.addEventListener('click', function(){
     profileClick.classList.toggle('profile-click')
 })
 
-// konfirmasi logout
-const logout = document.getElementById("logout");
-const konfirmasi = document.getElementById("confirm");
-const main = document.getElementById("main");
-const close = document.getElementById("close");
-logout.addEventListener('click', function(){
-    konfirmasi.style.display = 'flex';
-    main.style.display = 'none';
-})
-close.addEventListener('click', function(){
-    konfirmasi.style.display = 'none';
-    main.style.display = 'flex';
-})
-
 const x = 10;
