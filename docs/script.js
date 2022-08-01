a651b55a4703157648bcb5348f66da00e060eff5
SatrioUtomo13 <satrio.utomo130799@gmail.com>
2022-08-02 00:07:55 +0700

    add javascript and logout system

--- docs/script.js
+++ docs/script.js
@@ -5,4 +5,32 @@ profile.addEventListener('click', function(){
     profileClick.classList.toggle('profile-click')
 })
 
+
+// logout click
+const confirm = document.getElementById('confirm');
+const main = document.getElementById('main');
+const cancel = document.getElementById('cancel');
+// const profileLogout = document.getElementById('profile-logout');
+// profileLogout.addEventListener('click', function(){
+//     confirm.style.display = 'flex';
+//     main.style.display = 'none';
+// })
+
+const logout = document.getElementsByClassName('logout');
+for(let i = 0; i < logout.length; i++){
+    logout[i].addEventListener('click', function(){
+        confirm.style.display = 'flex';
+        main.style.display = 'none';
+    })
+}
+
+cancel.addEventListener('click', function(){
+    confirm.style.display = 'none';
+    main.style.display = 'flex';
+})
+
+
+
+
+
 const x = 10;
